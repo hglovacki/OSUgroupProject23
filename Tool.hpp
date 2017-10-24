739c8effa57fdba5c655c71885f10984500b3a14
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-24 19:50:03 -0400

    Update Tool.hpp

--- Tool.hpp
+++ Tool.hpp
@@ -1,1 +1,14 @@
 //
+
+//Include guard
+#ifndef TOOL_HPP
+#define TOOL_HPP
+
+class Tool
+{
+    protected:
+            int strength;
+            char type;
+     public:
+            void SetStrength(int);
+    
