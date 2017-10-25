378724a60dabdd9374502ee04b498108ca13a7fd
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 19:45:29 -0400

    Update RPSGame.hpp

--- RPSGame.hpp
+++ RPSGame.hpp
@@ -1,1 +1,20 @@
-//
+//Header File for the RPSGame class
+
+#include "Tool.hpp"
+#include "Rock.hpp"
+#include "Scissor.hpp"
+#include "Paper.hpp"
+
+class RPSGame
+{
+  protected:
+            Tool *human;
+            Tool *computer;
+            int humanWins = 0;
+            int computerWins = 0;
+            int ties = 0;
+  public:
+            
+};
+
+#endif
