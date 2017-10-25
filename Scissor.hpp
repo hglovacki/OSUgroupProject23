8cd52ef2404f85588d2d2e32ddc033a1f0683868
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 17:29:51 -0400

    Update Scissor.hpp

--- Scissor.hpp
+++ Scissor.hpp
@@ -1,1 +1,14 @@
-//
+//Scissor Class Header File
+//This class must inherit from the Tool class
+#ifndef SCISSOR_HPP
+#define SCISSOR_HPP
+
+class Scissor : public Tool
+{
+    public:
+          Scissor();
+          Scissor(int);
+          int fight(Tool);
+};
+
+#endif
