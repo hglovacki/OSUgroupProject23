20effd7093db4ff728db70225bd6293000bcacd9
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 17:43:11 -0400

    Update Rock.hpp

--- Rock.hpp
+++ Rock.hpp
@@ -1,1 +1,17 @@
-//
+//Rock Class Header File
+//This class must inherit from the Tool Class
+
+#ifndef ROCK_HPP
+#define ROCK_HPP
+
+#include "Tool.hpp"
+
+class Rock : public Tool
+{
+      public:
+              Rock();
+              Rock(int);
+              int figth(Tool);
+};
+
+#endif
