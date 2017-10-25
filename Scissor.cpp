d4e5b833f6e87bd0676c931fda4bb2a24444bc39
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 17:41:25 -0400

    Update Scissor.cpp

--- Scissor.cpp
+++ Scissor.cpp
@@ -1,1 +1,38 @@
-//
+//Scissor Class Implementation File
+//This class must inherit from the Tool class
+
+#include "Scissor.hpp"
+
+/*******************************************************
+                Default Constructor
+* This constructor will set the strength of this object to 1
+* and the type to 's'
+********************************************************/
+Scissor::Scissor()
+{
+  strength = 1;
+  type = 's';
+}
+
+/***********************************************************
+                Overlaoded Constructor
+* This constructor will set the strength of this object to
+* the value passed as a parameter and set the type to 's'
+*************************************************************/
+Scissor::Scissor(int newStrength)
+{
+  strength = newStrength;
+  type = 's';
+}
+
+/*************************************************************
+                    Scissor::fight(Tool)
+* This method will double the strength of this object if the type of
+* the Tool object passed to the parameter is 'p'. The strength
+* will halve if the type of the Tool object passed to the parameter 
+* is 'r'
+***************************************************************/
+int Scissor::fight(Tool opponent)
+{
+    
+}
