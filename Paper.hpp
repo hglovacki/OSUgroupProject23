304d4238bad893c48d9ca84598c0c829da72033d
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 16:44:40 -0400

    Update Paper.hpp

--- Paper.hpp
+++ Paper.hpp
@@ -1,1 +1,16 @@
-//
+//Paper Class header file
+//This class must inherit from the Tool class
+#ifndef PAPER_HPP
+#define PAPER_HPP
+
+#include "Tool.hpp"
+
+class Paper : public Tool
+{
+          public:
+                  Paper();
+                  Paper(int);
+                  int fight(Tool);
+};
+
+#endif
