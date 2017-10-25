45eb0aa778b7faa0e1424008879a6674a6b6f3ca
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 17:50:41 -0400

    Update Rock.cpp

--- Rock.cpp
+++ Rock.cpp
@@ -1,1 +1,38 @@
-//
+//Rock Class Implementation File
+//This class must inherit from the Tool class
+
+#include "Rock.hpp"
+
+/********************************************************
+                  Default Constructor
+* This constructor will set the strength of this object to 1
+* and the type to 'r'
+**********************************************************/
+Rock::Rock()
+{
+  strength = 1;
+  type = 'r';
+}
+
+/**********************************************************
+                   Overloaded Constructor
+* This constructor will take an int parameter and use it to 
+* set the strength of this object. It will also set the type 
+* to 'r'
+************************************************************/
+Rock::Rock(int newStrength)
+{
+  strength = newStrength;
+  type = 'r';
+}
+
+/************************************************************
+                    Rock::fight(Tool)
+* This function will temporarily double the strength of this object if the
+* type of the Tool object is 's' and will temporarily halve the strength of
+* this object if the type of the Tool object is 'p'
+*************************************************************/
+int Rock::fight(Tool opponent)
+{
+  
+}
