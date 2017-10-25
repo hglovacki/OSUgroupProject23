212215884eb30bb2d097c88b68cfd14e96649443
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-25 17:11:08 -0400

    Update Tool.cpp

--- Tool.cpp
+++ Tool.cpp
@@ -10,3 +10,33 @@ void Tool::SetStrength(int strong)
 {
    strength = strong;
 }
+
+/***************************************************
+*                 Tool::getStrength()
+* This method will return the value of the strength 
+* variable of this object
+****************************************************/
+int Tool::getStrength()
+{
+   return this->strength;
+}
+
+/********************************************************
+                  Tool::setType(char)
+* This method will take a char as a parameter and set it
+* to the type variable of the object that calls it
+**********************************************************/
+void Tool::setType(char newType)
+{
+   type = newType;
+}
+
+/**********************************************************
+                    Tool::getType()
+* This method will return the char value that is associated
+* with the type variable of the object that calls it
+************************************************************/
+char Tool::getType()
+{
+   return this->type;
+}
