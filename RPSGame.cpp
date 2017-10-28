21a5bf091ed4999085b0343d7d2cc29a7cf01ec0
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-28 15:22:44 -0400

    Update RPSGame.cpp

--- RPSGame.cpp
+++ RPSGame.cpp
@@ -8,4 +8,14 @@ using std::cin;
 using std::endl;
 using std::rand;
 
+/****************************************************************
+*                     RPSGame::menu()
+* This function will ask the user if they would like to play a round
+* of RPS or quit the program. It will take the user input and either quit,
+* or move on to the next step. Next it will ask the user which tool they
+* would like to use, then will randomly pick which tool the computer will
+* use.
+******************************************************************/
+
+
 
