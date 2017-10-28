1e4ec434e76d3d8eda537c585730c5056640f5aa
leavenss <33075385+leavenss@users.noreply.github.com>
2017-10-28 15:18:01 -0400

    Update RPSGame.cpp

--- RPSGame.cpp
+++ RPSGame.cpp
@@ -1,1 +1,11 @@
-//
+//Implementation file for the RPS Game class
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::rand;
+
+
